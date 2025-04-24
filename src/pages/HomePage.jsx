7503e140f7486922c1f1713fe0e19471006d8751
serk03 <onalserkan@googlemail.com>
2025-04-24 23:59:51 +0100

    setup home page to render components, added form with search functionlity

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,14 +1,34 @@
 import EventCard from "../components/EventCard";
+import { useState } from "react";
 
 function HomePage() {
+  const [searchQuery, setSearchQuery] = useState("");
+
   const events = [
     { id: 1, title: "Trent Park Trail", date: "25-05-2025" },
     { id: 2, title: "Montagu Park Trail", date: "26-05-2025" },
     { id: 3, title: "Regents Park Trail", date: "27-05-2025" },
   ];
 
+  const handleSearch = () => {
+    alert(searchQuery);
+  };
+
   return (
     <div className="home">
+      <form onSubmit={handleSearch} className="search-form">
+        <input
+          type="text"
+          placeholder="Search for Event.."
+          className="search-input"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+        />
+        <button type="submit" className="search-button">
+          Search
+        </button>
+      </form>
+
       <div className="events-grid">
         {events.map((event) => (
           <EventCard event={event} key={event.id} />
