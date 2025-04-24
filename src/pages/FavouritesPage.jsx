2b476f314040fc07178337d477a26275ec3cea58
serk03 <onalserkan@googlemail.com>
2025-04-25 00:37:07 +0100

    added intial setup structure for favourites page

---
+++ src/pages/FavouritesPage.jsx
@@ -0,0 +1,10 @@
+function FavouritesPage() {
+  return (
+    <div className="favourites-empty">
+      <h2>No Favourite Events Yet</h2>
+      <p>Start adding events to your favourites and they will appear</p>
+    </div>
+  );
+}
+
+export default FavouritesPage;
