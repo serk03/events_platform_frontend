fc218b042ab90bf5623fb90c3f57a8763019e8dd
serk03 <onalserkan@googlemail.com>
2025-04-25 01:22:21 +0100

    added page routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import { useState } from "react";
+import FavouritesPage from "./pages/FavouritesPage";
 import HomePage from "./pages/HomePage";
 import "./css/App.css";
 
 function App() {
   return (
-    <>
-      <HomePage />
-    </>
+    <main className="main-content">
+      <Routes>
+        <Route path="/" element={<HomePage />}></Route>
+        <Route path="/favourites" element={<FavouritesPage />}></Route>
+      </Routes>
+    </main>
   );
 }
 export default App;
