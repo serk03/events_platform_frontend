232401df73ae3d39d87fa6349e927cccff1e28db
serk03 <onalserkan@googlemail.com>
2025-04-25 19:28:46 +0100

    created initial navbar

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,18 @@
+function NavBar() {
+  return (
+    <nav className="navbar">
+      <div className="navbar-brand">
+        <Link to="/">Event App</Link>
+      </div>
+      <div className="navbar-links">
+        <link to="/" className="nav-link">
+          Home
+        </link>
+        <link to="/favourites" className="nav-link">
+          Favourites
+        </link>
+      </div>
+    </nav>
+  );
+}
+export default NavBar;
