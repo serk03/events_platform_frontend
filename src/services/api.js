b7532cb7be4bdab93c3158946c1dca956258e5cb
serk03 <onalserkan@googlemail.com>
2025-04-26 18:52:10 +0100

    made amendments to the fetchevent api

--- src/services/api.js
+++ src/services/api.js
@@ -1,11 +1,25 @@
-const API_KEY = "GAUTZQFKNNK7SX7DGP";
-const BASE_URL = "https://www.eventbriteapi.com/v3/";
+const BASE_URL = "https://www.eventbriteapi.com/v3";
+const API_TOKEN = "XMVZJIUFYHF5THBWTPJF"; //
 
-export const getPopularEvents = async () => {
-  const response = await fetch(`${BASE_URL}/events/popular?api_key=${API_KEY}`);
-  const data = await response.json();
-  return data.results;
-};
+export async function fetchEvents() {
+  try {
+    const response = await fetch(`${BASE_URL}/events/search/`, {
+      headers: {
+        Authorization: `Bearer ${API_TOKEN}`,
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    return data.events;
+  } catch (error) {
+    console.error("Failed to fetch events:", error);
+    return [];
+  }
+}
 
 export const searchEvents = async (query) => {
   const response = await fetch(
