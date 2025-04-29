96ffed6364e0b75584ca9fdc4d03f4cf7461a7d4
serk03 <onalserkan@googlemail.com>
2025-04-29 14:37:33 +0100

    added connection to backend using .env

--- src/pages/UserRegisterPage.jsx
+++ src/pages/UserRegisterPage.jsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
 import "../css/UserRegisterPage.css";
 
+const API_URL = import.meta.env.VITE_API_URL;
+
 function RegisterPage() {
   const [formData, setFormData] = useState({
     firstName: "",
@@ -33,7 +35,7 @@ function RegisterPage() {
     }
 
     try {
-      const response = await fetch("http://localhost:5000/api/auth/register", {
+      const response = await fetch(`${API_URL}/api/auth/register`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
