6e676d3d93f36972b11081c191cd07605254745a
serk03 <onalserkan@googlemail.com>
2025-05-06 03:24:29 +0100

    created and configured firebase file

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// src/firebase.js
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA_ya857JBdIBuYuajUBQs0zAtckGXnQP4",
+  authDomain: "event-platform-78e32.firebaseapp.com",
+  projectId: "event-platform-78e32",
+  storageBucket: "event-platform-78e32.appspot.com",
+  messagingSenderId: "709886380359",
+  appId: "1:709886380359:web:b819c0d21472898a652462",
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
